38bc2151939239a7dd81cad704f964217e8866f5
Tayeba Sultana <sultanatayeba43@gmail.com>
2025-05-08 08:34:44 +0600

    I did it! Problem four is done

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,33 @@ function concatenateArrays<T>(...arrays: T[][]): T[]{
     return [].concat(...arrays);
 }
 // console.log(concatenateArrays([1, 2], [3, 4], [5]));
+
+// Problem four
+class Vehicle {
+   private make: string;
+   private year:number;
+
+   constructor (make: string, year: number) {
+         this.make = make;
+         this.year = year;
+   }
+   public getInfo():string {
+     return `Make: ${this.make}, Year: ${this.year}`;
+   }
+}
+
+class Car extends Vehicle {
+    private modal: string;
+
+    constructor (make: string, year: number, modal: string){
+        super(make, year);
+        this.modal = modal;
+    }
+    public getModel(): string {
+        return `Modal: ${this.modal}`;
+    }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+console.log(myCar.getInfo());
+console.log(myCar.getModel());
